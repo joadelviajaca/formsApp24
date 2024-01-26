0113e50eac8ada0ed4ab18fa791cdeb227640cc5
joadelvia <joadelvia@gmail.com>
2024-01-26 07:21:37 +0100

    c

---
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dinamicos',
+  standalone: true,
+  imports: [],
+  templateUrl: './dinamicos.component.html'
+})
+export class DinamicosComponent {
+
+}
---
+++ src/app/template/switches/switches.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switches',
+  standalone: true,
+  imports: [],
+  templateUrl: './switches.component.html'
+})
+export class SwitchesComponent {
+
+}
