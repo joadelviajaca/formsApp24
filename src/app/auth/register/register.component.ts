09b2dff3329c770af5b5a0e36503276f771e1178
joadelvia <joadelvia@gmail.com>
2024-01-31 10:01:26 +0100

    email pattern

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+
+  constructor(private fb: FormBuilder){}
+
+  nameSurnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)'
+  emailPattern         : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.nameSurnamePattern)]],
+    email: ['', [Validators.required, Validators.email]],
+    login: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    confirmPassword: ['', [Validators.required]]
+  })
+
+  invalidField(field: string){
+    return this.myForm.get(field)?.invalid 
+            && this.myForm.get(field)?.touched;
+
+  }
+
+  submit(){
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value)
+  }
 
 }
