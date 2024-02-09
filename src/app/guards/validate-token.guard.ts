f8192f77847349c0ae2b475fbbcc1bbfe771a049
joadelvia <joadelvia@gmail.com>
2024-02-09 13:32:26 +0100

    guard

--- src/app/guards/validate-token.guard.ts
+++ src/app/guards/validate-token.guard.ts
@@ -1,5 +1,15 @@
+import { inject } from '@angular/core';
 import { CanMatchFn } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
 
 export const validateTokenGuard: CanMatchFn = (route, segments) => {
+  const authService = inject(AuthService);
+  authService.validateToken()
+  .subscribe(
+    {
+      next: (resp) =>  true,
+      error: (err) => false
+    }
+  )
   return true;
 };
